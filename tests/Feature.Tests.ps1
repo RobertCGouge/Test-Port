a206713123d36fd7c3977e081cf60b9116d104e1
Robert Gouge <robert.c.gouge@gmail.com>
2017-11-17 15:05:16 -0600

    Enable Feature Tests for Build

--- tests/Feature.Tests.ps1
+++ tests/Feature.Tests.ps1
@@ -3,7 +3,7 @@ $script:ModuleName = 'Test-Port'
 
 $ip = Resolve-DnsName -Name www.google.com | Where-Object -FilterScript {$_.Type -like 'AAAA'}
 
-Describe "Test-Port -Port parameter" {
+Describe "Test-Port -Port parameter" -Tags Build {
     It "Should throw an error on an invalid TCP port number" {
         {Test-port -Computer localhost -Port 65536} | should throw
     }
@@ -11,26 +11,26 @@ Describe "Test-Port -Port parameter" {
         {Test-port -Computer localhost -Port string | should throw}    
     }
 
-<# For some reason this test will not pass in AppVeyor.  It should pass locally if you uncomment it.
+    <# For some reason this test will not pass in AppVeyor.  It should pass locally if you uncomment it.
     It "Should not throw on a valid TCP port number" {
         {Test-port -Computer localhost -Port 80} | should not throw
     }
 #>
 }
 
-Describe "Test-Port -Computer parameter"{
+Describe "Test-Port -Computer parameter" -Tags Build {
     It "Should accecpt a string" {
         {Test-port -Computer localhost -Port 53 | should not throw}
     }
-    It "Should throw when passed a name that is unabled to be resolved"{
+    It "Should throw when passed a name that is unabled to be resolved" {
         {Test-Port -Computer notfound -Port 53 | should throw "$_ could not be resolved, please try another computer name."}
     }
-        It "Should throw when passed an ip address" {
+    It "Should throw when passed an ip address" {
         {Test-port -Computer 8.8.8.8 -Port 53 | should throw "$_ is an IP address, please try again with -IPv4"}
     }
 }
 
-Describe "Test-Port -IPv4"{
+Describe "Test-Port -IPv4" -Tags Build {
     It "Should throw when an invalid IPv4 is passed" {
         {test-port -IP 555.555.555.555 -port 53 | should throw "$_ is not a valid IPv4 Address"}
     }
