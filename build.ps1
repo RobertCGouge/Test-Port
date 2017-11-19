1cd4ba6688c87c0a68eb68614d8e9f3537a5dea6
Robert Gouge <robert.c.gouge@gmail.com>
2017-11-19 13:55:30 -0600

    Correct dependent modules

--- build.ps1
+++ build.ps1
@@ -11,11 +11,11 @@ Write-Output "Starting build"
 # Grab nuget bits, install modules, set build variables, start build.
 Write-Output "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Install-Module InvokeBuild, PSDeploy, BuildHelpers, PSScriptAnalyzer -force -Scope CurrentUser
+Install-Module InvokeBuild, PSDeploy, BuildHelpers, PSScriptAnalyzer, platyPS -force -Scope CurrentUser
 Install-Module Pester -Force -SkipPublisherCheck -Scope CurrentUser
 
 Write-Output "  Import Dependent Modules"
-Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer
+Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer, platyPS
 
 Set-BuildEnvironment
 
